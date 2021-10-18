12bc900f7cdb8c9a06fc32b28eb6e315b8c77411
cgx521 <3468546910@qq.com>
2021-10-18 23:39:17 +0800

    改动

--- IMissYou.cpp
+++ IMissYou.cpp
@@ -30,13 +30,27 @@ Q_OBJECT
 	public :
 	AWidget(QObject*parent=0);
 	private :
+singal: int 射门没() const;
 slots:
-	void display();
-	private :
-	QThread* thread;
-	QTimer* timer;
-	int count;
 
+
+};
+class Test1 : public QThread
+{
+
+
+	public 
+          slots:
+	  void test哈给我(int );
+	int mgw(QThread*obj);
+
+};
+class Test2: public QApplication
+{
+	Q_OBJECT
+	virtual void test()=0;
+	virtual int interfacex()=0 ;
+	virtual QObject* create()=0;
 };
 int main(int argc,char*argv[])
 {
