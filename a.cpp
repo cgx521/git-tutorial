b4568dcc90516d047cd357f4177ba4e50e4565dd
cgx521 <3468546910@qq.com>
2021-10-20 22:31:06 +0800

    a.cpp feature-C修改没问过

--- a.cpp
+++ a.cpp
@@ -9,3 +9,44 @@ int main(int argc,char*[]argv)
    int a=3,b=4;
     int &&b = a+b;
 }
+
+static void dismg()
+{
+
+	Msg * m = "Hello World!";
+	typdef struct MSG
+	{
+
+	   int id;
+	   char* msg;
+	   int size;
+	 } Msg ,*Handler;
+	class a : public QWidget 
+	{
+		a(QWidget*parent=0) QWidget(parent)
+		{ }
+		public slots:
+			void testSlots();
+	}
+}
+Item
+{
+   Rectangle
+   {
+
+id : red;
+color :"red";
+width: 25px; height:56;
+       anchors.fill:parent;
+       MouseArea
+       {
+
+
+	    onClicked: 
+	       {
+		       console.log("什么鬼");
+	       }
+   }
+
+
+};
