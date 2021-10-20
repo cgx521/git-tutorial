b61a4320f6d429eeaa7e06bf330b515501f7b97d
cgx521 <3468546910@qq.com>
2021-10-20 22:11:44 +0800

    a.cpp第一次提交 feature-A

--- a.cpp
+++ a.cpp
@@ -7,5 +7,19 @@ int main(int argc,char*[]argv)
    cout<<"测试Hello World!"<<endl;
 
    int a=3,b=4;
-    int &&b = a+b;
+  //  int &&b = a+b;
+}
+void dis()
+{
+	cout<<"我测试色各位"<<std::endl;
+std::vector<int> ve;
+   
+}
+template<typename Container,Int intType>
+void display(Container*con, Int size)
+{
+           for(Int a=0;a<size;++a)
+		   std::find_if(con.begin(),con.end();
+				   [&](Int a){ if(a)cout<<a<<endl;
+				   });
 }
